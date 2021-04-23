8aa12a018062566464eccf08b5881c55fc0dbe28
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-04-22 21:02:31 -0400

    Exercise 31 & 32 Completed

---
+++ GC_Exercise_31_32/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+namespace GC_Exercise_31_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise31();
+            Exercise32();
+        }
+
+        public static void Exercise31()
+        {
+            Console.WriteLine("Exercise 31");
+            int[] numbers = new int[] { 2, 8, 0, 24, 51 };
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("Enter an index of the array");
+
+                int index;
+
+                bool userInput = int.TryParse(Console.ReadLine(), out index);
+                
+                if(!userInput || index > numbers.Length)
+                {
+                    Console.WriteLine("That is not a valid index.");
+                }
+                else
+                    Console.WriteLine($"The value at index {index} is {numbers[index]}");
+
+                //continue?
+                bool conLoop = true;
+                while (conLoop)
+                {
+                    Console.WriteLine("Continue? y/n");
+                    string vCon = Console.ReadLine().ToLower();
+
+                    switch (vCon)
+                    {
+                        case "y":
+                            conLoop = false;
+                            repeat = true;
+                            break;
+                        case "n":
+                            Console.WriteLine("Goodbye!");
+                            conLoop = false;
+                            repeat = false;
+                            break;
+                        default:
+                            Console.WriteLine("I didn't catch that");
+                            conLoop = true;
+                            break;
+                    }
+                }
+            }
+
+        }
+
+
+
+        public static void Exercise32()
+        {
+            Console.WriteLine("Exercise 32");
+            int[] numbers = new int[] { 2, 8, 0, 24, 51 };
+
+            bool repeat = true;
+
+            while (repeat)
+            {
+                Console.WriteLine("Enter a number.");
+                int parsedInput;
+
+                bool userInput = int.TryParse(Console.ReadLine(), out parsedInput);
+
+
+                int index = Array.IndexOf(numbers, parsedInput);
+
+                if (numbers.Contains(parsedInput) == false || userInput == false)
+                {
+                    Console.WriteLine("That value cannot be found in the array.");
+                }
+                else
+                {
+                    Console.WriteLine($"The value {parsedInput} can be found at index {index}");
+                }
+
+                //continue?
+                bool conLoop = true;
+                while (conLoop)
+                {
+                    Console.WriteLine("Continue? y/n");
+                    string vCon = Console.ReadLine().ToLower();
+
+                    switch (vCon)
+                    {
+                        case "y":
+                            conLoop = false;
+                            repeat = true;
+                            break;
+                        case "n":
+                            Console.WriteLine("Goodbye!");
+                            conLoop = false;
+                            repeat = false;
+                            break;
+                        default:
+                            Console.WriteLine("I didn't catch that");
+                            conLoop = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
